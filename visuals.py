f4d8840e1265689cdbe83bb3bc0b60805d393741
José Romo <jose.e.romo@hotmail.com>
2026-02-19 00:04:57 -0600

    Add 3D LIDAR plotting function and update trends

--- visuals.py
+++ visuals.py
@@ -1,17 +1,29 @@
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import numpy as np
 
-def plot_comparative_trends(df_filtrado):
+def plot_3d_lidar(X, Y, Z):
+    fig = go.Figure(data=[go.Surface(z=Z, x=X, y=Y, colorscale='YlOrBr')])
+    fig.update_layout(
+        scene=dict(
+            zaxis=dict(range=[0, 15], title="Altura (m)"),
+            aspectratio=dict(x=1, y=1, z=0.4)
+        ),
+        margin=dict(l=0, r=0, b=0, t=0)
+    )
+    return fig
+
+def plot_comparative_trends(df_dia):
     fig = make_subplots(specs=[[{"secondary_y": True}]])
     
-    # Temperatura
-    fig.add_trace(go.Scatter(x=df_filtrado['Fecha_Hora'], y=df_filtrado['Temp_Ext'], name="Temp Ext (MTY)"), secondary_y=False)
-    fig.add_trace(go.Scatter(x=df_filtrado['Fecha_Hora'], y=df_filtrado['Temp_Int'], name="Temp Int (Bodega)"), secondary_y=False)
+    # Temperaturas (Eje Principal)
+    fig.add_trace(go.Scatter(x=df_dia['Fecha_Hora'], y=df_dia['Temp_Ext'], name="Temp Ext (MTY)"), secondary_y=False)
+    fig.add_trace(go.Scatter(x=df_dia['Fecha_Hora'], y=df_dia['Temp_Int'], name="Temp Int (Bodega)"), secondary_y=False)
     
-    # Humedad
-    fig.add_trace(go.Scatter(x=df_filtrado['Fecha_Hora'], y=df_filtrado['Hum_Int'], name="Hum Int (%)", line=dict(dash='dash')), secondary_y=True)
+    # Humedad (Eje Secundario)
+    fig.add_trace(go.Scatter(x=df_dia['Fecha_Hora'], y=df_dia['Hum_Int'], name="Hum Int (%)", line=dict(dash='dash', color='green')), secondary_y=True)
     
-    fig.update_layout(title="Análisis Térmico e Higrométrico", hovermode="x unified")
+    fig.update_layout(title="Análisis Térmico e Higrométrico (24h)", hovermode="x unified")
     fig.update_yaxes(title_text="Temperatura °C", secondary_y=False)
     fig.update_yaxes(title_text="Humedad %", secondary_y=True)
     return fig
